c30d8608f350d3fa7335ce5ab9d20bba64076c5d
philipauu <Phil@Phils-MacBook.local>
2017-10-03 15:18:01 +0800

    Misery loves company, but company does not reciprocate.

--- js/javascript.js
+++ js/javascript.js
@@ -9,27 +9,16 @@ var myGameArea = {
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     this.frameNo = 0;
     this.interval = setInterval(updateGameArea, 20);
-
-    window.addEventListener('keydown', function (e) {
-      myGameArea.keys = e.keyCode;
-    })
-    window.addEventListener('keyup', function (e) {
-      myGameArea.keys = false;
-  })
-},
+  },
 
   clear: function() {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  },
-
-  stop: function() {
-    clearInverval(this.interval);
   }
 }
 
 var myGamePiece;
-
-var myObstacles;
+var myObstacles = [];
+var myScore;
 
 $(document).ready(setup);
 
@@ -44,21 +33,40 @@ function startGame() {
   myObstacles = new component(10, 200, "yellow", 300, 120);
 }
 
-function component(width, height, color, x, y) {
+function component(width, height, color, x, y, type) {
+  this.type = type;
+  this.score = 0;
   this.width = width;
   this.height = height;
   this.speedX = 0;
   this.speedY = 0;
   this.x = x;
   this.y = y;
+  this.gravity = 0;
+  this.gravitySpeed = 0;
   this.update = function() {
     ctx = myGameArea.context;
-    ctx.fillStyle = color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    if (this.type == "text") {
+      ctx.font = this.width + " " + this.height;
+      ctx.fillStyle = color;
+      ctx.fillText(this.text, this.x, this.y);
+    } else {
+      ctx.fillStyle = color;
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
   }
   this.newPos = function() {
+    this.gravitySpeed += this.gravity;
     this.x += this.speedX;
-    this.y += this.speedY;
+    this.y += this.speedY + this.gravitySpeed;
+    this.hitBottom();
+  }
+  this.hitBottom = function() {
+    var bottom = myGameArea.canvas.height - this.height;
+    if (this.y > bottom) {
+      this.y = bottom;
+      this.gravitySpeed = 0;
+    }
   }
   this.crashWith = function(otherobj) {
     var myleft = this.x;
@@ -82,43 +90,46 @@ function component(width, height, color, x, y) {
   }
 }
 
-function moveup() {
-  myGamePiece.speedY -= 1;
+function updateGameArea() {
+
+  var x, height, gap, minHeight, maxHeight, minGap, maxGap;
+  for (counter = 0; counter < myObstacles.length; counter += 1) {
+    if (myGamePiece.crashWith(myObstacles[counter])) {
+      return;
+    }
+  }
+
+  myGameArea.clear();
+  myGameArea.frameNo += 1;
+  if (myGameArea.frameNo == 1 || everyinterval(150)) {
+    x = myGameArea.canvas.width;
+    minHeight = 20;
+    maxHeight = 200;
+    height = Math.floor(Math.random() * (maxHeight - minHeight + 1) + minHeight);
+    minGap = 50;
+    maxGap = 200;
+    gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
+    // myObstacles.push(new component(10, height, "yellow", x, 0));
+    // myObstacles.push(new component(10, x - height - gap, "yellow", x, height + gap));
+  }
+
+  for (counter = 0; counter < myObstacles.length; counter += 1) {
+    myObstacles[counter].x += -1;
+    myObstacles[counter].update();
+  }
+  // myScore.text="SCORE: " + myGameArea.frameNo;
+  // myScore.update();
+  myGamePiece.newPos();
+  myGamePiece.update();
 }
 
-function movedown() {
-  myGamePiece.speedY += 1;
-}
-
-function stopMove() {
-  myGamePiece.speedX = 0;
-  myGamePiece.speedY = 0;
-}
-
-function everyinterval(note) {
+function everyinterval(n) {
   if ((myGameArea.frameNo / n) % 1 == 0) {
     return true;
   }
   return false;
 }
 
-function updateGameArea() {
-  myGameArea.clear();
-  myGamePiece.speedX = 0;
-  myGamePiece.speedY = 0;
-  if (myGameArea.key && myGameArea.key == 37) {
-    myGamePiece.speedX = -1;
-  }
-  if (myGameArea.key && myGameArea.key == 39) {
-    myGamePiece.speedX = 1;
-  }
-  if (myGameArea.key && myGameArea.key == 38) {
-    myGamePiece.speedY = -1;
-  }
-  if (myGameArea.key && myGameArea.key == 40) {
-    myGamePiece.speedY = 1;
-  }
-  myObstacles.update();
-  myGamePiece.newPos();
-  myGamePiece.update();
+function accelerate(n) {
+  myGamePiece.gravity = n;
 }
