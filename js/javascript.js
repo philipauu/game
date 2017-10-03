6576d4711fb9f3d22cb9d8d619b525ef9c2ebd0d
philipauu <Phil@Phils-MacBook.local>
2017-10-03 14:02:18 +0800

    Total strangers need love, too; and I'm stranger than most.

--- js/javascript.js
+++ js/javascript.js
@@ -9,10 +9,19 @@ var myGameArea = {
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     this.frameNo = 0;
     this.interval = setInterval(updateGameArea, 20);
-  },
+
+    window.addEventListener('keydown', function (e) {
+      myGameArea.keys = e.keyCode;
+    })
+    window.addEventListener('keyup', function (e) {
+      myGameArea.keys = false;
+  })
+},
+
   clear: function() {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   },
+
   stop: function() {
     clearInverval(this.interval);
   }
@@ -81,19 +90,35 @@ function movedown() {
   myGamePiece.speedY += 1;
 }
 
-function everyinterval(note){
+function stopMove() {
+  myGamePiece.speedX = 0;
+  myGamePiece.speedY = 0;
+}
+
+function everyinterval(note) {
   if ((myGameArea.frameNo / n) % 1 == 0) {
-    return true;}
-    return false;
+    return true;
   }
+  return false;
+}
 
 function updateGameArea() {
-  if (myGamePiece.crashWith(myObstacles)) {
-    myGameArea.stop();
-  } else {
-    myGameArea.clear();
-    myGamePiece.newPos();
-    myGamePiece.update();
-    myObstacles.update();
+  myGameArea.clear();
+  myGamePiece.speedX = 0;
+  myGamePiece.speedY = 0;
+  if (myGameArea.key && myGameArea.key == 37) {
+    myGamePiece.speedX = -1;
   }
+  if (myGameArea.key && myGameArea.key == 39) {
+    myGamePiece.speedX = 1;
+  }
+  if (myGameArea.key && myGameArea.key == 38) {
+    myGamePiece.speedY = -1;
+  }
+  if (myGameArea.key && myGameArea.key == 40) {
+    myGamePiece.speedY = 1;
+  }
+  myObstacles.update();
+  myGamePiece.newPos();
+  myGamePiece.update();
 }
