bec54eccb9532c469b92158905c35d88fac8e031
philipauu <Phil@Phils-MacBook.local>
2017-10-09 11:59:42 +0800

    WRONG!

--- js/javascript.js
+++ js/javascript.js
@@ -21,14 +21,6 @@ var obstacles = [];
 var obstacle;
 var score;
 
-var current_player = true;
-var player_name_1 = 'Player 1',
-  player_name_2 = 'Player 2',
-  current_name = 'Player 1';
-var token_1 = 'Player 1',
-  token_2 = 'Player 2',
-  current_token = 'Player 1';
-
 $(document).ready(setup);
 
 function setup() {
@@ -154,9 +146,33 @@ function accelerate(shift) {
 function change_player() {
   console.log('Inside change_player');
 
-  console.log('swaps players AND token');
-  current_player = !current_player;
-  current_name = current_player ? player_name_1 : player_name_2;
-  current_token = current_player ? 'Player 1' : 'Player 2'
-  console.log('player is now: ', current_name);
+  public static int switchPlayer(int currentPlayer, boolean[] playerList) {
+      // if the current player + 1 = length (size) of array,
+      // start back at the beginning and find the first player still playing
+      if(currentPlayer + 1 == playerList.length) {
+          for(int i = 0; i < playerList.length; i++) {
+              if(playerList[i] == true) {    // if player is still in the game
+                  currentPlayer = i;         // currentPlayer = current index of array
+                  break;
+              }
+          }
+      }
+      // otherwise the current player number + 1 is not at the end of the array
+      // i.e. it is less than the length (size) of the array, so find the next player
+      // still playing
+      else {
+          for(int i = (currentPlayer+1); i < playerList.length; i++) {
+            if(currentPlayer + 1 == playerList.length) {
+    for(int i = 0; i < playerList.length; i++) {
+        if(playerList[i] == true) {
+            currentPlayer = i;
+            break;
+        }
+    }
+}
+              }
+          }
+      }
+      return currentPlayer;
+  }
 }
