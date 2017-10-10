dc5eadfb207ec1f07c74659ba035056c70edb298
philipauu <Phil@Phils-MacBook.local>
2017-10-10 11:28:15 +0800

    If you try to please everyone, somebody is not going to like it.

--- js/javascript.js
+++ js/javascript.js
@@ -149,8 +149,3 @@ function accelerate(shift) {
 //   gamerestart();
 //   ctx.fill
 // });
-
-$newDiv = $('<div>');
-$newDiv.html('Winner!');
-$newDiv.addClass('winner');
-$('#notices').append($newDiv);
